6d514ecf033ae661e6eecbe03149ac2d9e9bd409
Shashank Manohar <shashankmanohar1734@gmail.com>
2025-11-08 12:02:04 +0530

    core

--- server.js
+++ server.js
@@ -4,20 +4,25 @@ import dotenv from "dotenv";
 import mongoDB from "./Config/mongoDB.js";
 import adminRoutes from "./Routes/adminRoutes.js";
 import marksRoutes from "./Routes/marksRoute.js";
-const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 dotenv.config();
 
+const app = express();
+
+app.use(cors({
+  origin: "https://teamexcellentcareerinstitute.in",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
+app.use(express.json());
+
 mongoDB();
+
 app.get('/', (req, res) => res.send('Hello from backend!'));
 app.use('/api/admin', adminRoutes);
 app.use('/api/marks', marksRoutes);
 
-
-
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
